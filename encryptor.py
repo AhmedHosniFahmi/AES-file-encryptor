c0878c49654d3abc032043174147e242bbb31ea8
Ahmed Hosni <a7med7osni61@gmail.com>
2023-03-19 23:14:08 +0200

    Update and rename files_encryptor.py to encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -4,10 +4,13 @@ import argparse
 
 
 class FilesEncryptor:
-    def __init__(self, name, output_name):
+    def __init__(self, name, output_name='enc'):
         self.key = None
         self.name = name
-        self.output_name = output_name
+        self.output = output_name
+
+    def set_output(self, output):
+        self.output = output
 
     def set_key(self, key):
         self.key = key
@@ -19,34 +22,38 @@ class FilesEncryptor:
         self.name = encrypt(self.name.encode('utf-8').hex(), self.key)
         enc = encrypt(f, self.key)
 
-        with open(self.output_name, 'wb') as f:
+        with open(self.output, 'wb') as f:
             f.write(bytes.fromhex(self.name) + b'\n')
             f.write(bytes.fromhex(enc))
 
 
 class FilesDecryptor:
-
-    def __init__(self, name):
+    def __init__(self, name, output_name=None):
         self.key = None
         self.name = name
+        self.output = output_name
 
     def set_key(self, key):
         self.key = key
+
+    def set_output(self, output):
+        self.output = output
 
     def generateFile(self):
         with open(self.name, 'rb') as f:
             self.name = bytes.hex(f.readline())[:-2]
             f = bytes.hex(f.read())
 
-        self.name = self.removeNullCharacters(decrypt(self.name, self.key))
-        self.name = bytes.fromhex(self.name).decode('utf-8')
-        self.name = self.name.split('.')
-        self.name[0] = self.name[0] + '(1)'
-        self.name = '.'.join(self.name)
+        if self.output is None:
+            self.output = self.removeNullCharacters(decrypt(self.name, self.key))
+            self.output = bytes.fromhex(self.output).decode('utf-8')
+            self.output = self.output.split('.')
+            self.output[0] = self.output[0] + '(1)'
+            self.output = '.'.join(self.output)
 
         data = self.removeNullCharacters(decrypt(f, self.key))
 
-        with open(self.name, 'wb') as f:
+        with open(self.output, 'wb') as f:
             f.write(bytes.fromhex(data))
 
     def removeNullCharacters(self, data):
@@ -56,20 +63,15 @@ class FilesDecryptor:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--encrypt", help="Encrypt given file")
-parser.add_argument("-d", "--decrypt", help="Decrypt given file")
-parser.add_argument("-k", "--key", required=True)
-parser.add_argument("-o", "--output")
+parser.add_argument("-e", "--encrypt", help="encrypt given file")
+parser.add_argument("-d", "--decrypt", help="decrypt given file")
+parser.add_argument("-k", "--key", required=True, help="key for encryption/decryption")
+parser.add_argument("-o", "--output", help="name of output file")
 args = parser.parse_args()
 
 passed_args = True
-output_name = args.output
-
 if args.encrypt:
-    if output_name is None:
-        print('You need to enter -o [output filename]')
-        passed_args = False
-    else: file_manager = FilesEncryptor(args.encrypt, output_name)
+    file_manager = FilesEncryptor(args.encrypt)
 elif args.decrypt:
     file_manager = FilesDecryptor(args.decrypt)
 else:
@@ -77,16 +79,18 @@ else:
     print("Please specify either -e or -d")
 
 if passed_args:
-    key = args.key
-    valid = True
-    if len(key) != 32 and len(key) != 64 and len(key) != 48:
-        print("Key is not valid")
-        valid = False
+    if args.output is not None: file_manager.set_output(args.output)
 
+    key = args.key
     try:
         int(key, 16)
     except ValueError:
         print("Key is not valid. Key must be 32, 64 or 48 hexadecimal characters.")
+        valid = False
+
+    valid = True
+    if len(key) != 32 and len(key) != 64 and len(key) != 48:
+        print("Key is not valid")
         valid = False
 
     if valid:
